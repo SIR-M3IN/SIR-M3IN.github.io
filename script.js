864f2f5097d5f9b6a8ba042d3b48c4a21ba12595
SIR-M3IN <119602566+SIR-M3IN@users.noreply.github.com>
2024-03-05 14:31:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ window.addEventListener('deviceorientation', handleOrientation);
 const player = document.getElementById('player');
 let currentPositionX = window.innerWidth / 2;
 
+const ufoOptions = [
+    'images/ufo-for-website_blue.svg',
+    'images/ufo-for-website_green.svg',
+    'images/ufo-for-website_pink.svg',
+    'images/ufo-for-website_yellow.svg'
+];
+const randomUfo = ufoOptions[Math.floor(Math.random() * ufoOptions.length)];
+const playerPicture = document.getElementById('player').querySelector('img');
+playerPicture.src = randomUfo;
+
 function handleOrientation(event) {
     // Gyroscope controls
     const beta = event.beta;
     const newPositionX = window.innerWidth / 2 + beta * 10;
-
-    // Ensure the new position is within the window boundaries
     currentPositionX = Math.max(0, Math.min(newPositionX, window.innerWidth - player.clientWidth));
     
     player.style.left = currentPositionX + "px";
@@ -20,9 +28,17 @@ function createEnemy() {
         enemy.remove();
     });
 
+    const randomAsteroid = 'images/asteroidcrop.svg';
+
     const enemy = document.createElement('div');
     enemy.className = 'enemy';
-    enemy.style.left = `${Math.random() * (window.innerWidth - 30)}px`;
+    enemy.style.left = `${Math.random() * (window.innerWidth - 150)}px`;
+    enemy.style.top = `${-300}px`;
+
+    const enemyImage = document.createElement('img');
+    enemyImage.src = randomAsteroid;
+    enemy.appendChild(enemyImage);
+
     document.body.appendChild(enemy);
 
     const enemyInterval = setInterval(() => {
@@ -31,8 +47,12 @@ function createEnemy() {
             enemy.remove();
             clearInterval(enemyInterval);
         }
-    }, 100);
+    }, 50);
+    const playerUFO = document.getElementById('player').querySelector('img');
+    playerUFO.src = randomUfo;
 }
+
+
 
 
 function checkCollision() {
@@ -52,7 +72,6 @@ function checkCollision() {
         }
     });
 }
-
 
 function gameOver() {
     window.location.href = 'index.html';
